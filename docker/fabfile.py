5b7a383c06254a4533c7d35181c59697574c756c
root <root@vm172-31-0-20.ksc.com>
2018-07-05 14:21:20 +0800

    delete the remote info

--- docker/fabfile.py
+++ docker/fabfile.py
@@ -1,21 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from fabric.api import env, put, cd, run, lcd, local
-
-env.hosts = ['Chaos@zc.hehanlin.cn:9999']
-env.password = 'liuzhichao_1203'
-
-
-def rebuild():
-    with cd('/home/Chaos/hehanlin'):
-        put('./docker-compose.yml', 'docker-compose.yml')
-        put('./nginx.conf', 'nginx/nginx.conf', use_sudo=True)
-        run('docker-compose down')
-        run('docker-compose up -d')
+from fabric.api import lcd, local
 
 
 def deploy():
-    local("curl http://zc.hehanlin.cn:6800/delproject.json -d project=spider")
+    local("curl http://jd.hehanlin.cn:6800/delproject.json -d project=spider")
     with lcd('..'):
         local("scrapyd-deploy")
         local("rm -rf build project.egg-info setup.py")
